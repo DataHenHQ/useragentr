fa435c1731d90157715b46617032f0a5c319101d
Parama Danoesubroto <paramaw@gmail.com>
2022-06-01 20:15:48 +0300

    Made user-agent-type to be enum

--- src/generate.rs
+++ src/generate.rs
@@ -1,14 +1,22 @@
-use clap::Args;
+use clap::{Args, ArgEnum};
 #[derive(Args)]
 pub struct Generate {
-    #[clap(help = "What kind of user-agent to generate. Possible values: desktop, mobile, googlebot2")]
-    pub user_agent_type: String,
+    #[clap(arg_enum, help = "What kind of user-agent to generate.")]
+    pub user_agent_type: UserAgentType,
 
-    #[clap(long, short='n', help = "How many user agent strings to generate", default_value="1")]
+    #[clap(long, short='n', help = "How many user agent strings to generate.", default_value="1")]
     pub count: u64,
 
-    #[clap(long,  help = "Path to the user agent config path. If not specified, the default built-in user agent combinations will be used")]
+    #[clap(long,  help = "Path to the user agent config path. If not specified, the default built-in user agent combinations will be used.")]
     pub ua_config: Option<String>,
+}
+
+#[derive(ArgEnum, Clone, Debug)]
+#[clap(rename_all="snake_case")]
+pub enum UserAgentType {
+    Desktop,
+    Mobile,
+    Googlebot2,
 }
 
 impl Generate {
